27ea4d310ffaab86049e3ec7bfe58ef92728b921
aadityaagrawal <adityakuagrawal@gmail.com>
2024-01-31 00:16:17 +0530

    Background Images Change

--- src/Components/nav_bar.js
+++ src/Components/nav_bar.js
@@ -1,35 +1,41 @@
 import Nav from "react-bootstrap/Nav";
 import Navbar from "react-bootstrap/Navbar";
 import logo from "../Assets/Images/logo.svg";
-import { Container } from "react-bootstrap";
 
 function NavigationBar() {
   return (
     <Navbar expand="lg" className="px-5">
       <Navbar.Brand href="#home">
-        <div className="d-flex align-items-center">
-          <img src={logo} height={32} alt="Logo" className="mr-4"></img>
-          <p className="m-0 ml-2 fs-3 text-white"> &nbsp; Procejer</p>
+        <div className="d-flex align-items-baseline py-3">
+          <img src={logo} height={52} alt="Logo"></img>
         </div>
       </Navbar.Brand>
 
-      <Navbar.Toggle aria-controls="basic-navbar-nav" className="bg-light"/>
+      <Navbar.Toggle aria-controls="basic-navbar-nav" className="bg-white" />
       <Navbar.Collapse id="basic-navbar-nav" className="justify-content-end">
         <Nav>
-          
           <Nav.Link
             href="#features"
-            className="text-dark text-white fw-light border-right"
+            className="text-dark text-white fw-light border-right fs-5"
           >
             Features & Solutions
           </Nav.Link>
-          <Nav.Link href="#career" className="text-dark text-white fw-light border-right">
+          <Nav.Link
+            href="#career"
+            className="text-dark text-white fw-light border-right fs-5"
+          >
             Career
           </Nav.Link>
-          <Nav.Link href="#inquire" className="text-dark text-white fw-light border-right">
+          <Nav.Link
+            href="#inquire"
+            className="text-dark text-white fw-light border-right fs-5"
+          >
             Contact Us
           </Nav.Link>
-          <Nav.Link href="#about" className="text-dark text-white fw-light border-right">
+          <Nav.Link
+            href="#about"
+            className="text-dark text-white fw-light border-right fs-5"
+          >
             About Us
           </Nav.Link>
         </Nav>
